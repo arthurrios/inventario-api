1982e063b800e3193b769a9e0e4a4a77c6130088
arthur <arthur.rios007@gmail.com>
2024-08-17 13:12:40 -0300

    resolve rebase conflicts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { GoogleStrategy } from './strategies/google.strategy';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UserModule } from 'src/user/user/user.module';
+import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './strategies/jwt.strategy';
+
 import { ConfigService } from 'src/config/config.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
