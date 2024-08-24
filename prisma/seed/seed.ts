36660c60827ed5c53f727c4c181d646398036bc0
Eduardo Santana <edusfc15@hotmail.com>
2024-08-24 18:14:00 -0300

    feat: Add new fields to Supplier model and update seed data
    This commit adds new fields (email, phone, address) to the Supplier model in the Prisma schema. It also updates the seed data in the suppliers.ts file to include these new fields for each supplier.
    
    The purpose of this commit is to enhance the Supplier model by adding important contact information fields and ensuring that the seed data is up to date with the new schema changes.

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-// initialize Prisma Client
+// Initialize Prisma Client
 const prisma = new PrismaClient();
 
 const Categories = require('./data/categories');
 const Products = require('./data/products');
+const Suppliers = require('./data/suppliers');
+const Orders = require('./data/orders');
 
 async function main() {
+  // Seed categories
   await Promise.all(
     Categories.map(async (category) => {
       await prisma.category.upsert({
@@ -17,6 +20,7 @@ async function main() {
     }),
   );
 
+  // Seed products
   await Promise.all(
     Products.map(async (product) => {
       const category = await prisma.category.findUnique({
@@ -37,15 +41,49 @@ async function main() {
       });
     }),
   );
+
+  // Seed suppliers
+  await Promise.all(
+    Suppliers.map(async (supplier) => {
+      await prisma.supplier.upsert({
+        where: { id: supplier.id },
+        update: {},
+        create: supplier,
+      });
+    }),
+  );
+
+  // Seed orders
+  await Promise.all(
+    Orders.map(async (order) => {
+      await prisma.order.upsert({
+        where: { id: order.id },
+        update: {},
+        create: {
+          id: order.id,
+          date: order.date,
+          supplier: { connect: { id: order.supplierId } },
+          items: {
+            create: order.items.map(item => ({
+              product: { connect: { id: item.productId } },
+              quantity: item.quantity,
+              unit_price: item.unit_price,
+              status: item.status,
+            })),
+          },
+        },
+      });
+    }),
+  );
 }
 
-// execute the main function
+// Execute the main function
 main()
   .catch((e) => {
     console.error(e);
     process.exit(1);
   })
   .finally(async () => {
-    // close Prisma Client at the end
+    // Close Prisma Client at the end
     await prisma.$disconnect();
   });
